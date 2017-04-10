d4335de132bf23cff5cb695fbad480cd0dc1a4cc
Paraprax <parkerjohnston28@gmail.com>
2017-04-10 02:44:22 -0400

    Configuring root aspictures#index

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,20 +7,20 @@ class PicturesController < ApplicationController
    @picture = Picture.find(params[:id])
   end
 
-
   def new
     @picture = Picture.new
   end
 
   def create
     # make a new picture with what picture_params returns (which is a method we're calling)
-  @picture = Picture.new(picture_params)
-  if @picture.save
-    # if the save for the picture was successful, go to index.html.erb
-    redirect_to pictures_url
-  else
-    # otherwise render the view associated with the action :new (i.e. new.html.erb)
-    render :new
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # if the save for the picture was successful, go to index.html.erb
+      redirect_to pictures_url
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'pictures#index'
   get 'pictures' => 'pictures#index'
 
   post 'pictures' => 'pictures#create' # this is a new line of code
   get 'pictures/new' => 'pictures#new' # this is also a new line of code
 
   get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+
   # Ignore the comments below for now
   # They are just documentation
 end
